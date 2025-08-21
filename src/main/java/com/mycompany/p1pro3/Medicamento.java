4e1c423939a00484c675eab0f7ff91150b64c857
NicolasZH <nicolas.zarate.hernandez@est.una.ac.cr>
2025-08-21 15:02:47 -0600

    Agregué la clase medicamento

---
+++ src/main/java/com/mycompany/p1pro3/Medicamento.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.p1pro3;
+
+/**
+ *
+ * @author Nicolas ZH
+ */
+public class Medicamento {
+    
+}
