85f4ed4406464be000092cc3ed521e47586adc7b
KarenAvlo <karen.aviles.lopez@est.una.ac.cr>
2025-08-20 23:32:31 -0600

    Initial commit

---
+++ src/main/java/com/mycompany/p1pro3/P1pro3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.p1pro3;
+
+/**
+ *
+ * @author minar
+ */
+public class P1pro3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
